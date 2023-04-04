e006631c45824cf7b4e74923f449aa8e0c64c7d7
matetenov <matetenov@mts.ru>
2023-04-05 02:35:59 +0400

    cleanup

--- src/main/java/api/PostApi.java
+++ src/main/java/api/PostApi.java
@@ -1,14 +1,8 @@
 package api;
 
 import defaultSpecification.DefaultRequestSpecification;
-import io.qameta.allure.internal.shadowed.jackson.core.type.TypeReference;
-import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
 import io.restassured.response.Response;
-import lombok.SneakyThrows;
 import models.Post;
-
-import java.io.InputStream;
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -28,11 +22,5 @@ public class PostApi {
     public Response getAllPosts() {
         return given(DefaultRequestSpecification.defaultRequestSpec)
                 .get();
-    }
-    @SneakyThrows
-    public List<Post> getPosts(InputStream posts) {
-        ObjectMapper objectMapper = new ObjectMapper();
-            TypeReference<List<Post>> typeReference = new TypeReference<>() {};
-            return objectMapper.readValue(posts, typeReference);
     }
 }
--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -1,5 +1,4 @@
 package common;
 
 public abstract class BaseTest {
-
 }
